9becf266318a0d4454287933042e5cc2797b4319
Mo <mohammed4685@outlook.com>
2020-10-17 00:01:25 +0100

    add form improvements

--- frontend/src/components/dynamic-form.js
+++ frontend/src/components/dynamic-form.js
@@ -1,5 +1,68 @@
 import React from 'react'
+import ReactDOM from 'react-dom'
+function Heading(props){
+    return(
+        <div className="form-input heading">
+            <label for="heading">Heading:</label>
+            <br/>
+            <input type="text" name="heading"/>
+            <button onClick={(e)=>handleRemove(e)}>Delete</button>
+        </div>
+    )
+}
+function Image(props){
+    return(
+        <div className="form-input image">
+            <label for="image">Image:</label>
+            <br/>
+            <input type="file" name="image"/>
+            <button onClick={(e)=>handleRemove(e)}>Delete</button>
+        </div>
+    )
+}
+function Text(props){
+    return(
+        <div className="form-input text">
+            <label for="image">Text:</label>
+            <br/>
+            <input type="text" name="text"/>
+            <button onClick={(e)=>handleRemove(e)}>Delete</button>
+            <button onClick={(e)=>handleElementUp(e)}>Up</button>
+            <button onClick={(e)=>handleElementDown(e)}>Down</button>
 
+        </div>
+    )
+}
+function handleRemove(element){
+    element.target.parentElement.parentElement.remove()
+}
+
+function Video(props){
+    return(
+        <div className="form-input video">
+            <label for="video">Video:</label>
+            <br/>
+            <input type="file" name="video"/>
+            <button onClick={(e)=>this.handleRemove(e)}>Delete</button>
+         
+
+        </div>
+    )
+}
+function handleElementUp(element){
+    const headElement = document.getElementById("form-inputs")
+    console.log(headElement, element.target.parentElement)
+    const old_position=element.target.parentElement
+    headElement.insertBefore(element.target.parentElement, old_position)
+    old_position.remove()
+
+}
+function handleElementDown(element){
+    const headElement = document.getElementById("form-inputs")
+    const old_position=element.target
+    headElement.insertAfter(element.target, old_position)
+    old_position.remove()
+}
 /**@class - DynamicForm
  **@description- dynamic form that updates via JS
  * @since - 1.0.0
@@ -8,100 +71,101 @@ import React from 'react'
 export default class DynamicForm extends React.Component
 {
     constructor(props){
+        super(props)
         this.state={
             posts:[],
             message:null,
-            picker:document.getElementById("form-picker")
+            picker:null
         }
 
     }
 
     componentDidMount=()=>{
-        Axios.get('/blogs/all',
+        /*Axios.get('/blogs/all',
         (posts)=>{
             if(posts.length <= 0){
                 this.setState({message:"No posts found"})
             }
             this.setState({posts:posts})
-        })
+        })*/
     }
-    handleRemove=(element)=>{
-
-    }
-    handleAdd=(element)=>{
-        switch(element){
-            case "heading":
-                
+    
+    handleSubmit=(element)=>{
+        const elements = document.getElementsByClassName("form-input")
+        let data_array = []
+        for(element in elements){
+            data_array.push({
+                name:element.name,
+                data:element.value
+            })
         }
+        //axios.post('/blog/add', data_array)
 
     }
+    
+    
     handleClick=(text)=>{
-        switch(text){
-            case "heading":
-                this.state.picker.insertBefore(this.Heading, this.state.picker)
-            case "image":
-                this.state.picker.insertBefore(this.Image, this.state.picker)
-            case "video":
-                this.state.picker.insertBefore(this.Video, this.state.picker)
-                
+        const newNode = document.createElement("div")
+        const refElement = document.getElementById("form-picker")
+        const headElement = document.getElementById("form-inputs")
+        if(text == "heading"){
+            headElement.insertBefore(newNode, refElement)
+            ReactDOM.render(<Heading/>, newNode)
         }
-    }
-    Heading=(props)=>{
-        return(
-            <div className="form-heading">
-                <label for="heading">Heading:</label>
-                <br/>
-                <textarea name="heading" cols="50" rows="40"></textarea>
-            </div>
-        )
-    }
-    Image=(props)=>{
-        return(
-            <div className="form-image">
-                <label for="image">Image:</label>
-                <br/>
-                <input type="file" name="image"/>
-            </div>
-        )
-    }
-
-    Video=(props)=>{
-        return(
-            <div className="form-video">
-                <label for="video">Video:</label>
-                <br/>
-                <input type="file" name="video"/>
-            </div>
-        )
+        else if(text == "image"){
+            headElement.insertBefore(newNode, refElement)
+            ReactDOM.render(<Image/>, newNode)
+        }
+        else if(text == "video"){
+            headElement.insertBefore(newNode, refElement)
+            ReactDOM.render(<Video/>, newNode)
+        }
+        else if(text == "text"){
+            headElement.insertBefore(newNode, refElement)
+            ReactDOM.render(<Text/>, newNode)
+        }
     }
 
 
 
     render=()=>{
+        return(
         <div className="dynamic-form">
-            <h1>Your posts</h1>
-            <div className="form-posts">
-                <div className="form-picker">
-                    <div className="form-picker-img" onClick={this.handleClick}>
+            <h1>Add a post</h1>
+            <div id="form-inputs">
+                <div className="form-input">
+                    <label for="title">Title:</label>
+                    <input type="text" name="title"/>
+                </div>
+                <div className="form-input">
+                <label for="date">Date:</label>
+                    <input type="date" name="title"/>
+                </div>
+                <div className="form-input">
+                <label for="tags">Tags:</label>
+                    <input type="text" name="tags"/>
+
+                </div>
+                
+                <div id="form-picker">
+                    <div className="form-picker-img" onClick={(e)=>this.handleClick("image")}>
                         <i className="fa fa-image"/>
                         <p>Add an image</p>
                     </div>
-                    <div className="form-picker-code">
-                        <i className="fa fa-image"/>
-                        <p>Add code</p>
-                    </div>
-                    <div className="form-picker-text">
+                   
+                    <div className="form-picker-text" onClick={(e)=>this.handleClick("text")}>
                         <i className="fa fa-image"/>
                         <p>Add text</p>
                     </div>
-                    <div className="form-picker-heading" onClick={}>
+                    <div className="form-picker-heading" onClick={(e)=>this.handleClick("heading")}>
                         <i className="fa fa-image"/>
                         <p>Add a heading</p>
                     </div>
                 </div>
             </div>
-            <button type="submit">Submit</button>
+            <button type="button">Submit</button>
         </div>
+        )
 
     }
 
---
+++ frontend/src/pages/add-page.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import DynamicForm from '../components/dynamic-form'
+
+/**@class - AddPage
+ * @description - used to add posts via dynamic form 
+ */
+export default class AddPage extends React.Component
+{
+    constructor(){
+        super()
+        
+    }
+    componentDidMount=()=>{
+       
+    }
+
+    render=()=>{
+        return(
+        <DynamicForm/>
+        )
+    }
+    
+}
