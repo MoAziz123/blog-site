d4d5b40de93480fd79942b3671085fc46ab28278
Mo <mohammed4685@outlook.com>
2021-01-10 21:47:48 +0000

    more server changes

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const jwt = require('jsonwebtoken')
-const path - require('path')
+const path = require('path')
 /***LIBRARIES***/
 const js_yaml = require('js-yaml')
 const fs = require('fs')
